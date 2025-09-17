67e404b20d31643fed0410f24fb0d4c884d7e4cc
DryckAz <azeitonagameryt@gmail.com>
2025-09-17 20:52:50 -0300

    Update aula7.py

--- aula7.py
+++ aula7.py
@@ -5,14 +5,15 @@
 # atribuir um valor a um nome (variável).
 # Uso: nome_variavel = expressão
 
-# nome_completo = 'Luiz Otávio Miranda'
+# nome_completo = 'Enndrick Azeitona'
 # soma_dois_mais_dois = 2 + 2
 # int_um = bool('1')
 # print(int_um, type(int_um))
 # print(nome_completo, soma_dois_mais_dois)
 
-nome = 'Luiz'
-idade = 17
+nome = 'Endrick Eduardo de Azevedo Azeitona'
+idade = 27
 maior_de_idade = idade >= 18
 print('Nome:', nome, 'Idade:', idade)
+
 print('É maior?', maior_de_idade)
